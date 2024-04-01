8a5bdc4370226f1331f1903ad508283f03db247b
shota kaseda <pocopoco79@gmail.com>
2024-04-01 14:01:27 +0900

    photo機能のベース実装
    - active_strage用テーブル構築(bin/rails active_storage:install 実施)
    - photosテーブル追加
    - photoの基本機能実装(バリデーション未実装)

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,28 @@
 
 class PhotosController < ApplicationController
   before_action :logged_in_user
+
   def index
+    @photos = current_user.photos
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = current_user.photos.new(photo_params)
+
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
 
-  resources :photos
+  resources :photos, only: [:index, :new, :create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
