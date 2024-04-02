f65c8aab5ed60dfc09d7a4af39990e98d12de117
shota kaseda <pocopoco79@gmail.com>
2024-04-02 21:25:52 +0900

    OAuth認証によるアクセストークン取得
    - OAuthの設定情報をConstantsに切り出し
    - viewにてOAuth認可ページのリンクを追加
    - controllerで認可codeを使いアクセストークン取得、sessionで保管

---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+module Constants
+  OAUTH_CONFIG = {
+    auth_base_url: 'http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com',
+    client_id: '36BnvMESjerQQPaPjvLyIPqnto-8Lf9af2PYT9UxpTY',
+    client_secret: 'vM4h9P-hqwtmBxDXm94IJ1aoW8QUfcOg3YeWSzfNwUY',
+    redirect_uri: 'http://localhost:3000/oauth/callback',
+  }.freeze
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   resources :photos, only: [:index, :new, :create]
 
+  # OAuth認可のコールバック用
+  get "/oauth/callback", to: "oauth_sessions#receive_oauth_response"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
